af7feadb132bd6e0923920b3cd1d6d81ef7c555b
st64134 <st64134@upce.cz>
2024-02-20 14:18:09 +0100

    uvodni commit

---
+++ src/test/java/com/example/NNPIA_cv_2/NnpiaCv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NNPIA_cv_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaCv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
